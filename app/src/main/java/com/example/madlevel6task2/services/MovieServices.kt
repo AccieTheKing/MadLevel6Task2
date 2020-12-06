c6c6f5cd8f9038bdf2e516ae9edf188e0d91431d
Acdaling <acdaling@gmail.com>
2020-12-06 16:05:19 +0100

    Created an Movie services that uses the api key to get list of movies

---
+++ app/src/main/java/com/example/madlevel6task2/services/MovieServices.kt
@@ -0,0 +1,11 @@
+package com.example.madlevel6task2.services
+
+import com.example.madlevel6task2.BuildConfig
+import com.example.madlevel6task2.models.Movie
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieServices {
+    @GET("/movie")
+    suspend fun getProfile(@Query("api_key") api_key: String = BuildConfig.MOVIE_API_KEY): List<Movie>
+}
